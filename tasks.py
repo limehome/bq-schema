c7b89c3f71c441593bceaa0465d23c1215846611
Raphael Brand <raphael.brand@limehome.de>
2020-09-22 11:34:49 +0200

    add code quality tasks

---
+++ tasks.py
@@ -0,0 +1,36 @@
+from invoke import task
+
+_TEST_FOLDER = "tests"
+_SOURCE_FOLDERS = " ".join(["bq_schema_placeholder", _TEST_FOLDER])
+
+
+@task
+def lint(context):
+    context.run(f"pylint {_SOURCE_FOLDERS}")
+
+
+@task
+def type_check(context):
+    context.run("mypy -m bq_schema_placeholder")
+
+
+@task
+def check_code_format(context):
+    context.run("black --check .")
+    context.run("isort --check .")
+
+
+@task
+def test(context):
+    context.run(f"pytest {_TEST_FOLDER}")
+
+
+@task
+def format_code(context):
+    context.run("black .")
+    context.run("isort .")
+
+
+@task(pre=[lint, type_check, check_code_format, test])
+def check_all(_):
+    pass
