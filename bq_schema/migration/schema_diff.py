ef9282c313e4ff6ddec3d42c2edc5f571156bce9
Bozhidar Stefanov <bojidar.stefanov@stanga.net>
2021-01-18 13:38:31 +0200

    Add legacy to standard field types mapping

--- bq_schema/migration/schema_diff.py
+++ bq_schema/migration/schema_diff.py
@@ -1,8 +1,11 @@
 from typing import Iterator, List, Optional
 
 from google.cloud import bigquery
+from google.cloud.bigquery.schema import _STRUCT_TYPES, LEGACY_TO_STANDARD_TYPES
+from google.cloud.bigquery_v2 import types
 
 
+# pylint: disable=no-member
 def check_schemas(
     local_schema: List[bigquery.SchemaField],
     remote_schema: List[bigquery.SchemaField],
@@ -15,12 +18,24 @@ def check_schemas(
         if column.name not in remote_columns:
             yield f"Nested: New column {column}" if is_nested else f"New column {column}"
             continue
-
         modes_not_equal = column.mode != remote_columns[column.name].mode
-        field_types_not_equal = (
-            column.field_type != remote_columns[column.name].field_type
+        column_field_type = LEGACY_TO_STANDARD_TYPES.get(
+            column.field_type, types.StandardSqlDataType.TypeKind.TYPE_KIND_UNSPECIFIED
         )
-        if modes_not_equal or field_types_not_equal:
+        remote_column_field_type = LEGACY_TO_STANDARD_TYPES.get(
+            remote_columns[column.name].field_type,
+            types.StandardSqlDataType.TypeKind.TYPE_KIND_UNSPECIFIED,
+        )
+        field_types_not_equal = column_field_type != remote_column_field_type
+
+        if types.StandardSqlDataType.TypeKind.TYPE_KIND_UNSPECIFIED in (
+            column_field_type,
+            remote_column_field_type,
+        ):
+            check_schema_msg = (
+                f"Unspecified field type in {column} or {remote_columns[column.name]}"
+            )
+        elif modes_not_equal or field_types_not_equal:
             check_schema_msg = (
                 f"Nested: There is difference between {column} and {remote_columns[column.name]}"
                 if is_nested
@@ -29,7 +44,7 @@ def check_schemas(
             yield check_schema_msg
             continue
 
-        if column.field_type == "RECORD":
+        if column.field_type in _STRUCT_TYPES:
             yield from check_schemas(
                 column.fields, remote_columns[column.name].fields, True
             )
