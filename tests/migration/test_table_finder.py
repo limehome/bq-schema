0e2e76c65861bc16a1e0d3b5b65e3d475919a4e0
bayraktarbaris <baris4111@gmail.com>
2022-05-18 15:03:50 +0200

    Fix: Added failing get_type_hints test case

--- tests/migration/test_table_finder.py
+++ tests/migration/test_table_finder.py
@@ -10,3 +10,11 @@ def test_table_finder():
     table_names = {t.name for t in find_tables(tables_dir)}
     expected_table_names = {"first_table", "second_table"}
     assert expected_table_names == table_names
+
+
+def test_table_schema_fields():
+    file_path = pathlib.Path(__file__).parent
+    tables_dir = os.path.join(file_path, "tables")
+    for local_table in find_tables(tables_dir):
+        schema_fields = local_table.get_schema_fields()
+        assert len(schema_fields) > 0
